55e0994885f56376784f362daa88c179c048f1ca
lesnedy <lesnedycharles@gmail.com>
2025-04-22 19:58:40 +0300

    Add files via upload

---
+++ ck.py
@@ -0,0 +1,17 @@
+print ("WELCOME MUST ")
+print("ENTER REGISTRATION NUMBER: ")
+reg=input("Reg:No: ")
+print("Enter password: ")
+pss=input("password: ")
+print("WELCOME MUST PAGE:")
+print("Enter registration:")
+regi=input("reg:no:")
+print("Enter Password:")
+pas=input("password")
+if pss==pas:
+    print("Login successful: ")
+else:
+    print("Fail to login")
+    
+    print("GPA calculation: ")
+    
